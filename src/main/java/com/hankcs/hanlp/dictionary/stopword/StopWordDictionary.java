3fdf98dea09218c04874d8cdb37828809739c3d8
liuliming <1328858024@qq.com>
2019-11-26 11:40:51 +0800

    没有任何修改。

--- src/main/java/com/hankcs/hanlp/dictionary/stopword/StopWordDictionary.java
+++ src/main/java/com/hankcs/hanlp/dictionary/stopword/StopWordDictionary.java
@@ -12,13 +12,11 @@
 package com.hankcs.hanlp.dictionary.stopword;
 
 import com.hankcs.hanlp.collection.MDAG.MDAGSet;
-import com.hankcs.hanlp.dictionary.common.CommonDictionary;
 import com.hankcs.hanlp.seg.common.Term;
 
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
 import java.util.Collection;
-
-import static com.hankcs.hanlp.utility.Predefine.logger;
 
 /**
  * @author hankcs
