b95997824c71104c390abbd73035ea84a633dfff
LimingLiu <1328858024@qq.com>
2019-12-30 16:36:39 +0800

    添加注释

--- src/test/java/com/hankcs/KeywordExtraction/Global.java
+++ src/test/java/com/hankcs/KeywordExtraction/Global.java
@@ -8,19 +8,19 @@ package com.hankcs.KeywordExtraction;
 public class Global {
 	// 测试集目录
 	public static final String testDataSetDir =
-			"F:\\2019学业相关资料\\17127122刘立明关键词提取数据集\\ShenCeBei2018\\test";
+			"F:\\2019学业相关资料\\17127122刘立明关键词提取数据集\\shnews\\test";
 	// 测试集的关键词结果
 	public static final String testDataSetAnswerFile =
-			"F:\\2019学业相关资料\\17127122刘立明关键词提取数据集\\ShenCeBei2018\\test-answer\\test_answer.txt";
+			"F:\\2019学业相关资料\\17127122刘立明关键词提取数据集\\shnews\\test-answer\\test_answer.txt";
 	// TextRank关键词提取结果
 	public static final String textRankResultsFile =
-			"F:\\2019学业相关资料\\17127122刘立明关键词提取数据集\\ShenCeBei2018\\TextRank\\textRank_answer.txt";
+			"F:\\2019学业相关资料\\17127122刘立明关键词提取数据集\\shnews\\TextRank\\textRank_answer.txt";
 	// PositionRank关键词提取结果
 	public static final String positionRankResultsFile =
-			"F:\\2019学业相关资料\\17127122刘立明关键词提取数据集\\ShenCeBei2018\\PositionRank\\positionRank_answer.txt";
+			"F:\\2019学业相关资料\\17127122刘立明关键词提取数据集\\shnews\\PositionRank\\positionRank_answer.txt";
 	// 改进型PositionRank关键词提取结果
 	public static final String improvedPositionRankResultFile =
-			"F:\\2019学业相关资料\\17127122刘立明关键词提取数据集\\ShenCeBei2018\\ImprovedPositionRank\\improvedPositionRank_answer.txt";
+			"F:\\2019学业相关资料\\17127122刘立明关键词提取数据集\\shnews\\ImprovedPositionRank\\improvedPositionRank_answer.txt";
 	/**
 	 * 提取关键词数目，默认为5个
 	 */
